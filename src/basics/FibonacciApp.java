31002ef598a623b2408dba43cd58ae779ba66c3b
Chris Young <chrisyoung0101@gmail.com>
2019-05-30 16:42:37 -0500

    First commit

---
+++ src/basics/FibonacciApp.java
@@ -0,0 +1,30 @@
+package basics;
+
+public class FibonacciApp {
+
+	public static void main(String[] args) {
+		// Fibonacci number is defined by the sum of the two previous Fibonacci numbers (similar to factorials)
+		// fib(0) = 0
+		// fib(1) = 1
+		// fib(2) = fib(1) + fib(0) = 0 + 1 = 1
+		// fib(3) = fib(2) + fib(1) = 1 + 1 = 2
+		// fib(4) = fib(3) + fib(2) = 2 + 1 = 3
+		// fib(5) = fib(4) + fib(3) = 3 + 2 = 5
+		
+		System.out.println(fib(5));
+		
+		
+	}
+	
+	public static int fib(int n) {
+		if (n == 0) {
+			return 0;
+		} 
+		else if (n == 1) {
+			return 1;
+		}
+		//finds the Fibonacci number -  this is recursion 
+		return ((fib(n-1)) + (fib(n-2)));
+	}
+
+}
